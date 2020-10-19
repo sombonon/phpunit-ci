db03c66d392e4e7f7df83e26a6b09fc508e80706
Somboon Nontaganok <somboonnon@outlook.com>
2020-10-19 15:15:20 +0700

    initial commit

---
+++ tests/Feature/BasicTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Box;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BasicTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testBoxContents() 
+    {
+        $box = new Box(['toy']);
+        $this->assertTrue($box->has('toy'));
+        $this->assertFalse($box->has('ball'));
+    }
+
+    public function testTakenOneFromTheBox()
+    {
+        $box = new Box(['torch']);
+        $this->assertEquals('torch', $box->takeOne());
+
+        // Null, now the box is empty
+        $this->assertNull($box->takeOne());
+    }
+
+    public function testStartsWithALetter()
+    {
+        $box = new Box(['toy', 'torch', 'ball', 'cat', 'tissue']);
+
+        $results = $box->startsWith('t');
+
+        $this->assertCount(3, $results);
+        $this->assertContains('toy', $results);
+        $this->assertContains('torch', $results);
+        $this->assertContains('tissue', $results);
+
+        // Emty array if passed even
+        $this->assertEmpty($box->startsWith('s'));
+
+    }
+}
